af563cfad1e534d3fe859e272e0ab89b983e2b79
Max Zanoga <zanoga.maxim@gmail.com>
2023-05-15 18:23:33 -0700

    update req file, add docs uploder to pinecone

--- docs2ai/upload_docs.py
+++ docs2ai/upload_docs.py
@@ -1,6 +1,11 @@
 import os
+
+import pinecone
 from dotenv import load_dotenv
 from langchain.document_loaders import ReadTheDocsLoader
+from langchain.embeddings import OpenAIEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain.vectorstores import Pinecone
 
 
 def read_docs():
@@ -9,7 +14,20 @@ def read_docs():
     return docs_to_load
 
 
+def split_docs(docs):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=100,
+                                                   separators=["\n\n", "\n", " ", ""])
+    splitted_chunks = text_splitter.split_documents(docs)
+    return splitted_chunks
+
+
 if __name__ == "__main__":
     load_dotenv()
-    print(os.getenv("PATH_TO_DATA"))
-    print(len(read_docs()))
+    pinecone.init(api_key=os.getenv("PINECONE_API_KEY"), environment=os.getenv("PINECONE_ENVIRONMENT_REGION"))
+    raw_docs = read_docs()
+    chunks = split_docs(raw_docs)
+
+    embeddings = OpenAIEmbeddings()
+    Pinecone.from_documents(chunks, embeddings, index_name=os.getenv("PINECONE_INDEX"), overwrite=True)
+
+    print("Uploaded docs to Pinecone!")
