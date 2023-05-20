5b3e2feba40d73c90a7d8444763bfce1a3a2768f
Max Zanoga <zanoga.maxim@gmail.com>
2023-05-19 20:31:43 -0700

    update req file and ui

--- docs2ai/ui.py
+++ docs2ai/ui.py
@@ -4,11 +4,6 @@ from streamlit_chat import message
 
 st.title("Docs to Chat AI")
 st.header("Docs to Chat AI")
-hide_st_style = """
-footer { display: none; }
-header { display: none; !important }
-"""
-st.markdown(hide_st_style, unsafe_allow_html=True)
 
 prompt = st.text_input("Enter your question here")
 
